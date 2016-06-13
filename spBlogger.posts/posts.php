c1ebd4f7a0559fae377550f5b3730470340242db
MIguel A. Torres <miguelfreelancer56577@gmail.com>
2016-06-13 08:44:10 -0500

    Add files via upload

---
+++ spBlogger.posts/posts.php
@@ -0,0 +1,15 @@
+<?php
+    print_r(json_encode(array(array(  'id'=> 2,
+                    'title'=> 'Simple title2',
+                    'content'=> 'Sample content...',
+                    'permalink'=> 'simple-title2',
+                    'author'=> 'Sandeep',
+                    'datePublished'=> '2012-05-04'
+                    ), array(  'id'=> 3,
+                    'title'=> 'Simple title3',
+                    'content'=> 'Sample content...',
+                    'permalink'=> 'simple-title3',
+                    'author'=> 'Sandeep',
+                    'datePublished'=> '2012-05-04'
+                    ))));
+?>
